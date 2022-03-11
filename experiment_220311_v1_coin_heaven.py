fcaf93d3fc4b585830f31edc13fa756dd2acc20e
Georg Grab <georg.grab@optellum.com>
2022-03-11 20:55:33 +0100

    milestone: 220311: consistently solve coin heaven with v1. but not optimally.

--- experiment_220311_v1_coin_heaven.py
+++ experiment_220311_v1_coin_heaven.py
@@ -6,25 +6,21 @@ from stable_baselines3.common.env_util import make_vec_env
 from bombergym.scenarios import coin_heaven
 
 from bombergym.environments import register
-from bombergym.environments.callbacks import CustomCallback
+from experiments.monitoring import CustomCallback
 
 register()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--checkpoint-dir', required=True) 
+parser.add_argument('--experiment', help='Description of the experiment', required=True) 
 parser.add_argument('--total-timesteps', required=False, type=int, default=1000000) 
 
 args = parser.parse_args()
 
-# Instantiate Custom Callback for checkpointing and evaling model
-if args.checkpoint_dir:
-    callback = CustomCallback(args.checkpoint_dir)
-else:
-    callback = None
-
 settings, agents = coin_heaven()
 
 env = make_vec_env("BomberGym-v1", n_envs=4, env_kwargs={'args': settings, 'agents': agents})
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=args.total_timesteps, callback=callback)
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=f"{args.checkpoint_dir}/{args.experiment}/")
+callback = CustomCallback(args.checkpoint_dir, args.experiment)
+model.learn(total_timesteps=args.total_timesteps, callback=callback, tb_log_name=args.experiment)
 model.save("bombermodel")
