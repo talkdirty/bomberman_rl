fd3fc6969d815791c8e6df4bb41bc004b93fd6c0
Georg Grab <georg.grab@optellum.com>
2022-03-13 16:06:18 +0100

    fix: original/agents: do not log to files, breaks parallellism

--- bombergym/original/agents.py
+++ bombergym/original/agents.py
@@ -199,14 +199,14 @@ class AgentRunner:
         self.wlogger.setLevel(s.LOG_AGENT_WRAPPER)
         self.fake_self.logger = logging.getLogger(self.agent_name + '_code')
         self.fake_self.logger.setLevel(s.LOG_AGENT_CODE)
-        log_dir = f'agent_code/{self.code_name}/logs/'
-        if not os.path.exists(log_dir): os.makedirs(log_dir)
-        handler = logging.FileHandler(f'{log_dir}{self.agent_name}.log', mode="w")
-        handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s: %(message)s')
-        handler.setFormatter(formatter)
-        self.wlogger.addHandler(handler)
-        self.fake_self.logger.addHandler(handler)
+        #log_dir = f'agent_code/{self.code_name}/logs/'
+        #if not os.path.exists(log_dir): os.makedirs(log_dir)
+        #handler = logging.FileHandler(f'{log_dir}{self.agent_name}.log', mode="w")
+        #handler.setLevel(logging.DEBUG)
+        #formatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s: %(message)s')
+        #handler.setFormatter(formatter)
+        #self.wlogger.addHandler(handler)
+        #self.fake_self.logger.addHandler(handler)
 
     def process_event(self, event_name, *event_args):
         module_name = None
